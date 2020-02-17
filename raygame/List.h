0d9628fcc7aa8eb01c2b7e0ac2f186b0724bb88f
Brionna Franklin <s198017@students.aie.edu>
2020-02-17 10:47:25 -0600

    This might be right? maybe?

--- raygame/List.h
+++ raygame/List.h
@@ -11,27 +11,92 @@ protected:
 	Node<T>* m_first;
 	Node<T>* m_last;
 
+	bool isEmpty
+
 public:
+
 	List();
-	List(List<T>&)
 	~List();
+	int length();
+	void front();
+	void back();
+	void insertFirst(const T&);
+	void insertLast(const T&);
+	void push(Node* head_ref, T NewDate);
+	void pushBack(Node<T>* head_ref, T NewDate)
 
-	//int Length();
-
-	T front();
-	T back();
-
-	//virtual bool search(const T&) = 0;
-	virtual void insertFirst(const T& first) = 0;
-	virtual void insertLast(const T& last) = 0;
-	//virtual void deleteNode(const T&) = 0;
-
-	//iterator<T> Begin();
-	//iterator<T> End();
-
-	bool isEmpty();
-
-private:
-	//void copyList(List<T> &)
 };
+
+template<class T>
+void List<T>::front()
+{
+	return startPtr;
+}
+
+template<class T>
+void List<T>::back()
+{
+	return endptr;
+}
+
+template<class T>
+void List<T>::insertFirst(const T &)
+{
+	if (isEmpty())
+	{
+		startPtr = newPtr;
+		endptr = newPtr;
+	}
+	else
+	{
+		Node<T>* newPtr = new Node<>(dataIn);
+
+		startPtr = newPtr;
+		endptr = newPtr;
+	}
+
+}
+
+
+template<class T>
+void List<T>::insertLast(const T &)
+{
+	if (isEmpty())
+	{
+		startPtr = newPtr;
+		endptr = newPtr;
+	}
+	else
+	{
+
+		Node<T>* newPtr = new Node<>(dataIn);
+
+		startPtr = newPtr;
+		endptr = newPtr;
+	}
+}
+
+template<class T>
+void List<T>::push(Node<T>* head_ref, T NewDate)
+{
+	Node new_node = new Node();
+
+	new_node->Node<T> = NewDate;
+
+	new_node->next = (*head_ref);
+
+	(*head_ref) = new_node;
+}
+
+template<class T>
+void List<T>::pushBack(Node<T>* End_ref, T NewDate)
+{
+	Node new_node = new Node();
+
+	new_node->Node<T> = NewDate;
+
+	new_node->next = (*End_ref);
+
+	(*End_ref) = new_node;
+}
 
