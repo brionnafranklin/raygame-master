f40b79dadc4ee7ba694fe2fe9be6277575b21955
Brionna Franklin <54289307+brionnafranklin@users.noreply.github.com>
2020-03-13 09:43:19 -0500

    Update Node.h

--- raygame/Node.h
+++ raygame/Node.h
@@ -1,23 +1,31 @@
 #pragma once
 
+//pieces of data that is put into lists
 template<typename T>
 class Node
 {
 public:
+	//default constructor
 	Node();
+	//default deconstructor
 	~Node();
 
+	//the information that is stored in a node
 	T info;
 
+	//the node that is next in the list
 	Node<T> * next;
+	//the node that is previous in the list
 	Node<T> * previous;
 };
 
+//default constructor
 template<typename T>
 inline Node<T>::Node()
 {
 }
 
+//default deconstructor
 template<typename T>
 inline Node<T>::~Node()
 {
