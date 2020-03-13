47ccad963fa91d354c724c791873067c00fd3dfc
Brionna Franklin <54289307+brionnafranklin@users.noreply.github.com>
2020-03-13 00:24:11 -0500

    Update Iterator.h

--- raygame/Iterator.h
+++ raygame/Iterator.h
@@ -1,42 +1,60 @@
 #pragma once
 #include "Node.h"
 
+
 template<typename T>
-class Iterator {
+//used to move through the list
+class Iterator 
+{
 private:
+	//declares a node
 	Node<T>* currentNode;
 
 public:
+	//returns the next thing in the list
 	Iterator<T> operator++(int);
+	//returns the previous thing in the list
 	Iterator<T> operator--(int);
 
+	//returns the data at that point in the list
 	T operator*();
 
+	//default constructor
 	Iterator();
+	//default deconstructor
 	~Iterator();
+	//creates a pointer to the current node
 	Iterator(Node<T>* ptr);
 
+	//checks to see if the currentNode matches inputted another node
 	bool operator==(const Iterator<T>&)const;
+	//checks to see if the currentNode does not match another inputted node
 	bool operator!=(const Iterator<T>&)const;
 };
 
+//returns the next thing in the list
 template<typename T>
 Iterator<T> Iterator<T>::operator++(int)
 {
+	//if next node isn't null
 	if (currentNode->next != nullptr)
 	{
 		currentNode = currentNode->next;
+		//returns the next node
 		return *this;
 	}
 	return nullptr;
 }
 
+//returns the previous thing in the list
 template<typename T>
 Iterator<T> Iterator<T>::operator--(int)
 {
+	//if previous node is not null
 	if (currentNode->previous != nullptr)
 	{
 		currentNode = currentNode->previous;
+		//returns the previous node
 		return *this;
 	}
 	return nullptr;
